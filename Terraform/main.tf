4f5cd081bd2bee417a7b73671edb81555a9fca9c
keerthi <keerthi@123>
2025-05-06 12:43:50 +0530

    userdatachange

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance_1"{
     ami = var.ami_value
     instance_type = var.instance_type_value
     key_name = var.key_value
+    user_data = base64encode(file("userdata1.sh"))
     tags = {
         Name = "Web server1"
     }
@@ -11,6 +12,7 @@ resource "aws_instance" "instance_2"{
     ami= var.ami_value
     instance_type = var.instance_type_value
     key_name = var.key_value
+    user_data = base64encode(file("userdata2.sh"))
     tags = {
         Name = "Web server2"
     }
