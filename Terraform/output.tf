1288afb611b14e0c78fd27e01ff4e17fff58ecfd
keerthi <keerthi@123>
2025-05-06 12:37:16 +0530

    fouth commit

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -8,11 +8,11 @@ output "public_ip_instance2"{
     value = aws_instance.instance_2.public_ip
 }
 
-output "instance_details"{
+output "instance_details1"{
     description ="this tells the first instance details"
     value = aws_instance.instance_1.id
 }
-output "instance_details"{
+output "instance_details2"{
     description ="this tells the first instance details"
     value = aws_instance.instance_2.id
 }
