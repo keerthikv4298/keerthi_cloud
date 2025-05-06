2347d520d49d1d6c6205748ac6ab2d6653b30bbe
keerthi <keerthi@123>
2025-05-06 12:34:17 +0530

    third commit

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -7,3 +7,12 @@ output "public_ip_instance2"{
     description = "this is the value of webserver2"
     value = aws_instance.instance_2.public_ip
 }
+
+output "instance_details"{
+    description ="this tells the first instance details"
+    value = aws_instance.instance_1.id
+}
+output "instance_details"{
+    description ="this tells the first instance details"
+    value = aws_instance.instance_2.id
+}
